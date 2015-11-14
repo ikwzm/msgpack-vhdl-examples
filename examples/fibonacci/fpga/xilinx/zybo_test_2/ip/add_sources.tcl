84ee51921003e345fe75e8e47d0408f57f3de7a3
KAWAZOME Ichiro <ichiro_k@ca2.so-net.ne.jp>
2015-11-14 10:29:22 +0900

    add examples/fibonacci/fpga/xilinx/zybo_test_2

---
+++ examples/fibonacci/fpga/xilinx/zybo_test_2/ip/add_sources.tcl
@@ -0,0 +1,3 @@
+add_vhdl_file sources_1 WORK ../../../../src/main/synthesijer/Fib.vhd
+add_vhdl_file sources_1 WORK ../../../../src/main/synthesijer/fib_interface.vhd
+add_vhdl_file sources_1 WORK ../../../../src/main/synthesijer/fibonacci_server.vhd
