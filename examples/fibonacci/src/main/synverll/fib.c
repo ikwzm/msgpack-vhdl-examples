f84d6d868bcfcd89b1ff63a05fe65ea6220325d4
KAWAZOME Ichiro <ichiro_k@ca2.so-net.ne.jp>
2015-11-16 15:25:13 +0900

    bug fix examples/fibonacci/src/main/synverll/fib.c

--- examples/fibonacci/src/main/synverll/fib.c
+++ examples/fibonacci/src/main/synverll/fib.c
@@ -5,10 +5,10 @@ int fib(int n)
   int i;
   if (n <= 0) return 0;
   if (n == 1) return 1;
-  for(i = 0; i < n; i++) {
+  for(i = 1; i < n; i++) {
     int prev_r1 = r1;
     r1 = r0 + r1;
     r0 = prev_r1;
   }
-  return r0;
+  return r1;
 }
