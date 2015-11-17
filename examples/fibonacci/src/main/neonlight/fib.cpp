d3dc2e68f5ae6c804a62f7b4923b4bb5ae248578
ikwzm <ichiro_k@ca2.so-net.ne.jp>
2015-11-18 07:40:39 +0900

    change fib.n

--- examples/fibonacci/src/main/neonlight/fib.cpp
+++ examples/fibonacci/src/main/neonlight/fib.cpp
@@ -8,17 +8,17 @@ public:
 
   int state;
   uint64_t r1_main;
-  uint64_t r4_main;
-  uint64_t reg_t3;
+  uint64_t r3_main;
   uint64_t reg_t5;
+  uint64_t reg_t7;
+  uint64_t reg_t1_v1;
   uint64_t reg_t2_v4;
   uint64_t reg_t3_v5;
-  uint64_t reg_t3_v6;
   uint64_t r4_main_v11;
   uint64_t reg_t1_v13;
   uint64_t reg_t2_v14;
   uint64_t reg_t3_v15;
-  uint64_t reg_t9;
+  uint64_t reg_t8;
   uint64_t reg_t10;
   void s_0();
   void s_57();
@@ -33,12 +33,12 @@ public:
 };
 
 void Mod_main::s_0() {
-  r1_main = 0UL;
+  r3_main = 0UL;
   reg_t2_v4 = 1UL;
-  reg_t3_v5 = 0UL;
-  reg_t5 = 0UL;
-  reg_t9 = 1UL;
-  reg_t10 = 0UL;
+  r1_main = 0UL;
+  reg_t8 = 1UL;
+  reg_t5 = 1UL;
+  reg_t10 = 1UL;
   if (true) {
     state = 57;
   }
@@ -70,14 +70,14 @@ void Mod_main::s_43() {
   uint64_t wire_t12;
   uint64_t wire_t13;
   uint64_t wire_t14;
-  wire_t11 = reg_t5 ? r4_main : r1_main;
-  wire_t12 = reg_t9 ? reg_t2_v4 : reg_t3;
-  wire_t13 = reg_t10 ? reg_t3_v6 : reg_t3_v5;
+  wire_t11 = reg_t8 ? r3_main : reg_t1_v1;
+  wire_t12 = reg_t5 ? reg_t2_v4 : reg_t7;
+  wire_t13 = reg_t10 ? r1_main : reg_t3_v5;
   wire_t14 = r4_main_v11 > wire_t13;
   reg_t1_v13 = wire_t11;
   reg_t2_v14 = wire_t12;
   reg_t3_v15 = wire_t13;
-  reg_t9 = wire_t14;
+  reg_t5 = wire_t14;
   if (true) {
     if (wire_t14) state = 48;
     else
@@ -86,17 +86,17 @@ void Mod_main::s_43() {
 }
 
 void Mod_main::s_48() {
-  uint64_t wire_t16;
   uint64_t wire_t15;
-  r4_main = reg_t2_v14;
-  reg_t5 = 1UL;
+  uint64_t wire_t16;
+  reg_t1_v1 = reg_t2_v14;
+  reg_t8 = 0UL;
   wire_t15 = reg_t2_v14 + reg_t2_v14;
-  reg_t10 = 1UL;
-  reg_t3 = wire_t15;
+  reg_t10 = 0UL;
+  reg_t7 = wire_t15;
   wire_t16 = reg_t3_v15 + 1UL;
-  reg_t9 = 0UL;
+  reg_t5 = 0UL;
   r1_main = wire_t15;
-  reg_t3_v6 = wire_t16;
+  reg_t3_v5 = wire_t16;
   if (true) {
     state = 43;
   }
--- examples/fibonacci/src/main/neonlight/fib.v
+++ examples/fibonacci/src/main/neonlight/fib.v
@@ -14,17 +14,17 @@ module mod_main(clk, rst, addr_o, write_en_o, data_o, data_i);
   reg [31:0] data_o;
 
   reg [31:0]  r1_main;
-  reg [31:0]  r4_main;
-  reg [31:0]  reg_t3;
+  reg [31:0]  r3_main;
   reg  reg_t5;
+  reg [31:0]  reg_t7;
+  reg [31:0]  reg_t1_v1;
   reg [31:0]  reg_t2_v4;
   reg [31:0]  reg_t3_v5;
-  reg [31:0]  reg_t3_v6;
   reg [31:0]  r4_main_v11;
   reg [31:0]  reg_t1_v13;
   reg [31:0]  reg_t2_v14;
   reg [31:0]  reg_t3_v15;
-  reg  reg_t9;
+  reg  reg_t8;
   reg  reg_t10;
   wire [31:0]  wire_t11;
   wire [31:0]  wire_t12;
@@ -84,11 +84,11 @@ module mod_main(clk, rst, addr_o, write_en_o, data_o, data_i);
   assign o_insn_103_0 = data_i;
   assign o_insn_105_0 = data_i;
   wire [31:0] o_insn_95_0; // id:95
-  assign o_insn_95_0 = reg_t5 ? r4_main : r1_main;
+  assign o_insn_95_0 = reg_t8 ? r3_main : reg_t1_v1;
   wire [31:0] o_insn_98_0; // id:98
-  assign o_insn_98_0 = reg_t9 ? reg_t2_v4 : reg_t3;
+  assign o_insn_98_0 = reg_t5 ? reg_t2_v4 : reg_t7;
   wire [31:0] o_insn_101_0; // id:101
-  assign o_insn_101_0 = reg_t10 ? reg_t3_v6 : reg_t3_v5;
+  assign o_insn_101_0 = reg_t10 ? r1_main : reg_t3_v5;
   wire o_insn_67_0; // id:67
   assign o_insn_67_0 = gt_6_d0;
   wire [31:0] o_insn_74_0; // id:74
@@ -114,19 +114,19 @@ module mod_main(clk, rst, addr_o, write_en_o, data_o, data_i);
     end else begin
 `ifdef NLI_DEBUG
       $display("NLI:st mod_main_main=%d", cur_st);
-      $display("NLI: r1_main =%d r4_main =%d reg_t3 =%d reg_t5 =%d reg_t2_v4 =%d reg_t3_v5 =%d reg_t3_v6 =%d r4_main_v11 =%d reg_t1_v13 =%d reg_t2_v14 =%d reg_t3_v15 =%d reg_t9 =%d reg_t10 =%d" ,r1_main ,r4_main ,reg_t3 ,reg_t5 ,reg_t2_v4 ,reg_t3_v5 ,reg_t3_v6 ,r4_main_v11 ,reg_t1_v13 ,reg_t2_v14 ,reg_t3_v15 ,reg_t9 ,reg_t10);
+      $display("NLI: r1_main =%d r3_main =%d reg_t5 =%d reg_t7 =%d reg_t1_v1 =%d reg_t2_v4 =%d reg_t3_v5 =%d r4_main_v11 =%d reg_t1_v13 =%d reg_t2_v14 =%d reg_t3_v15 =%d reg_t8 =%d reg_t10 =%d" ,r1_main ,r3_main ,reg_t5 ,reg_t7 ,reg_t1_v1 ,reg_t2_v4 ,reg_t3_v5 ,r4_main_v11 ,reg_t1_v13 ,reg_t2_v14 ,reg_t3_v15 ,reg_t8 ,reg_t10);
 `endif
       // state output
       write_en_o <= (cur_st == `S_14);
       // FSM.
       case (cur_st)
         `S_0: begin
-          r1_main <= 32'd0;
+          r3_main <= 32'd0;
           reg_t2_v4 <= 32'd1;
-          reg_t3_v5 <= 32'd0;
-          reg_t5 <= 2'd0;
-          reg_t9 <= 2'd1;
-          reg_t10 <= 2'd0;
+          r1_main <= 32'd0;
+          reg_t8 <= 2'd1;
+          reg_t5 <= 2'd1;
+          reg_t10 <= 2'd1;
           addr_o <= 0;
           cur_st <= `S_57;
         end
@@ -146,20 +146,20 @@ module mod_main(clk, rst, addr_o, write_en_o, data_o, data_i);
           reg_t1_v13 <= wire_t11;
           reg_t2_v14 <= wire_t12;
           reg_t3_v15 <= wire_t13;
-          reg_t9 <= wire_t14;
+          reg_t5 <= wire_t14;
           if (wire_t14) begin
             cur_st <= `S_48;
           end else
           cur_st <= `S_14;
         end
         `S_48: begin
-          r4_main <= reg_t2_v14;
-          reg_t5 <= 2'd1;
-          reg_t10 <= 2'd1;
-          reg_t3 <= wire_t15;
-          reg_t9 <= 2'd0;
+          reg_t1_v1 <= reg_t2_v14;
+          reg_t8 <= 2'd0;
+          reg_t10 <= 2'd0;
+          reg_t7 <= wire_t15;
+          reg_t5 <= 2'd0;
           r1_main <= wire_t15;
-          reg_t3_v6 <= wire_t16;
+          reg_t3_v5 <= wire_t16;
           cur_st <= `S_43;
         end
       endcase
@@ -172,15 +172,4 @@ module mod_main(clk, rst, addr_o, write_en_o, data_o, data_i);
   // sub modules begin
   // sub modules end
 
-endmodule
-
-module fib(clk, rst, addr_o, write_en_o, data_o, data_i);
-  input clk;
-  input rst;
-  // EXTERNAL_RAM
-  output [31:0] addr_o;
-  output write_en_o;
-  output [31:0] data_o;
-  input [31:0] data_i;
-  mod_main mod_main_inst(.clk(clk), .rst(rst), .addr_o(addr_o), .write_en_o(write_en_o), .data_o(data_o), .data_i(data_i));
 endmodule
