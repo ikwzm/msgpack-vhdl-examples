dc97010c0ab66380eb7822bcdac88e50180942c9
KAWAZOME Ichiro <ichiro_k@ca2.so-net.ne.jp>
2016-01-21 12:16:24 +0900

    Update examples/fibonacci/fpga/xilinx/zybo_test_vhdl/project and ip

--- examples/fibonacci/fpga/xilinx/ip/add_sources.tcl
+++
@@ -1,3 +0,0 @@
-add_vhdl_file sources_1 WORK ../../../src/main/vhdl/fib.vhd
-add_vhdl_file sources_1 WORK ../../../src/main/vhdl/fib_interface.vhd
-add_vhdl_file sources_1 WORK ../../../src/main/vhdl/fibonacci_server.vhd
