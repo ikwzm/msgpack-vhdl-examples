272a6e026ce6eed7541480706528b5e0cd3f3af7
KAWAZOME Ichiro <ichiro_k@ca2.so-net.ne.jp>
2016-01-21 18:05:07 +0900

    Update examples/fibonacci/fpga/xilinx/zybo_test_vivado_hls/project and ip

---
+++ examples/fibonacci/fpga/xilinx/zybo_test_vivado_hls/project/export_hardware.tcl
@@ -0,0 +1,3 @@
+set     project_directory   [file dirname [info script]]
+set     project_name        "project"
+source  [file join ".." ".." ".." ".." ".." ".." "lib" "fpga" "xilinx" "zybo_test_1" "project" "export_hardware.tcl"]
--- examples/fibonacci/fpga/xilinx/zybo_test_vivado_hls/project/implementation.tcl
+++ examples/fibonacci/fpga/xilinx/zybo_test_vivado_hls/project/implementation.tcl
@@ -1,15 +1,3 @@
-
-set project_directory       [file dirname [info script]]
-set project_name            "project"
-
-open_project [file join $project_directory $project_name]
-launch_runs synth_1
-wait_on_run synth_1
-launch_runs impl_1
-wait_on_run impl_1
-open_run    impl_1
-report_utilization -file [file join $project_directory "project.rpt" ]
-report_timing      -file [file join $project_directory "project.rpt" ] -append
-launch_runs impl_1 -to_step write_bitstream -job 4
-wait_on_run impl_1
-close_project
+set     project_directory   [file dirname [info script]]
+set     project_name        "project"
+source  [file join ".." ".." ".." ".." ".." ".." "lib" "fpga" "xilinx" "zybo_test_1" "project" "implementation.tcl"]
