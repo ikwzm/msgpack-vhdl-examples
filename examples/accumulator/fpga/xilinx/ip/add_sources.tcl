684a92c40697b823f87e2f8b661b11ad651634e3
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2016-06-24 19:38:46 +0900

    [update] msgpack-vhdl 0.2(draft) for examples/accumulator

--- examples/accumulator/fpga/xilinx/ip/add_sources.tcl
+++ examples/accumulator/fpga/xilinx/ip/add_sources.tcl
@@ -1,5 +1,3 @@
 add_vhdl_file sources_1 WORK ../../../src/main/synthesijer/Accumulator.vhd
-add_vhdl_file sources_1 WORK ../../../src/main/vhdl/add_interface.vhd
-add_vhdl_file sources_1 WORK ../../../src/main/vhdl/get_interface.vhd
-add_vhdl_file sources_1 WORK ../../../src/main/vhdl/set_interface.vhd
+add_vhdl_file sources_1 WORK ../../../src/main/vhdl/Accumulator_Interface.vhd
 add_vhdl_file sources_1 WORK ../../../src/main/vhdl/accumulator_server.vhd
